8658cc43698787330eec3782a1daf118ef2a75f8
m-mma <mark.muema@gmail.com>
2018-09-11 07:46:26 +0300

    updated the gulp file and added css tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,23 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('workflow', function () {
+  gulp.src('sass/*.scss')
+
+  .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'],
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('stylesheets/'))
+
+
+  .pipe(gulp.dest('stylesheets/'))
+});
+
+gulp.task('default', function () {
+  gulp.watch('sass/*.scss', ['workflow']);
+});
