21fe3923204a616e28365ec58aee69947433a199
m-mma <mark.muema@gmail.com>
2018-09-11 21:22:57 +0300

    added common css gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var gulp = require('gulp');
 var gulpwatch = require('gulp-watch');
 // var monitorCtrlC = require('monitorctrlc');
@@ -8,8 +7,6 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
-
-
 
 //Watch for 'Ctrl + C' on Windows and end gulp process
 if (process.platform === "win32") {
@@ -26,112 +23,36 @@ process.on("SIGINT", function () {
   // graceful shutdown
   process.exit();
 });
+// Start CSS tasks
+gulp.task('css-tasks', function () {
+	gulp.src('sass/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'],
+			cascade: false
+		}))
+		.pipe(cssnano())
+		.pipe(sourcemaps.write('./'))
 
-// start gulp tasks
-
-gulp.task('sass', function(){
-  return gulp.src('sass/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('stylesheets/'))
-
+	.pipe(gulp.dest('stylesheets/'))
 });
 
-gulp.task('watch', function(){
-  gulp.watch('sass/**/*.scss', ['sass']);
+gulp.task('default', function () {
+	gulp.watch('sass/**/*.scss', ['css-tasks']);
 });
+// End CSS tasks
 
-gulp.task('default', ['watch']);
-
-
-// gulp.task('default', ['sass','watch']);
-
-// gulp.task('sass', function(done){
-//   return gulp.src('sass/*.scss')
-//     .pipe(sass())
-//     .pipe(gulp.dest('stylesheets/'))
-//     done();
-// });
-
-
-// gulp.task('watch', function(done){
-//   return watch('sass/*.scss', ['sass']);
-//    done();
-// });
-
-// gulp.task('default', ['sass','watch']);
-
-// gulp.task('watch', function(){
-//   gulp.watch('sass/*.scss', {cwd: './'}, ['sass']);
-// });
-
+// Start gulp tasks
 // gulp.task('sass', function(){
 //   return gulp.src('sass/*.scss')
-//   return watch('sass/*.scss', { ignoreInitial: false })
 //     .pipe(sass())
 //     .pipe(gulp.dest('stylesheets/'))
 // });
-
-// gulp.task('default', ['sass']);
-
-
-
+//
 // gulp.task('watch', function(){
-//   return watch('sass/*.scss', function () {
-//         gulp.src('sass/*.scss')
-//             .pipe(gulp.dest('stylesheets/'));
-//     });
+//   gulp.watch('sass/**/*.scss', ['sass']);
 // });
 //
-
-
-// gulp.task('watch', function(done){
-//   gulp.watch('sass/*.scss', ['sass']);
-//   done();
-// });
-
-// Watch on windows
-// gulp.task('sass', function(done) {
-//   done();
-// });
-// gulp.task('watch', function(done) {
-//   done();
-// });
-
-
-// gulp.task('default',['sass']);
-
-
-// gulp.task ('sass', function() {
-//     return gulp.src ('./sass/*.scss')
-//       .pipe(sass())
-//       .pipe(gulp.dest('./stylesheets'));
-//   }
-// )
-// gulp.task('default',['sass']);
-
-// gulp.task('workflow', function () {
-//   gulp.src('./sass/*.scss')
-//
-//   .pipe(sourcemaps.init())
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(autoprefixer({
-//       browsers: ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'],
-//       cascade: false
-//     }))
-//     .pipe(cssnano())
-//     .pipe(sourcemaps.write('./'))
-//
-//
-//   .pipe(gulp.dest('./stylesheets/dist/'))
-// });
-//
-// gulp.task('default', function () {
-//   gulp.watch('./sass/*.scss', ['workflow']);
-
-//
-// });
-
-// gulp.task('test', function() {
-//   console.log('test task');
-// });
-// gulp.task('default',['hello','sass']);
+// gulp.task('default', ['watch']);
+// End gulp tasks
