1fe575fd99782cbdc22fe3c958a4de035a215ed0
m-mma <mark.muema@gmail.com>
2018-09-11 07:28:01 +0300

    added some gulp plugins and a gulp js file

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
